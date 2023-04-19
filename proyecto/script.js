63b5836f25ea07436ca8d6e9910b25d6e5bc795d
Ediison32 <edisonalvarez32@gmail.com>
2023-04-19 00:19:06 -0500

    concatenar html_ofertas

--- proyecto/script.js
+++ proyecto/script.js
@@ -4,8 +4,12 @@ const menuEmail = document.querySelector('.emai_nav');
 const one = document.querySelector('.insert_one');
 const offmenu = () =>{
         const menuStoreStado = three.classList.contains('display');
+        const menuProduct = five.classList.contains('display');
         if (!menuStoreStado){
                 three.classList.add('display');
+        }
+        if(!menuProduct){
+                five.classList.add('display');
         }
         one.classList.toggle("display");
 }
@@ -13,15 +17,32 @@ const offmenu = () =>{
 
 
 /* insert_two = hamburgesa  */
-
+const insert_fourt = document.querySelector('.insert_fourt')
 const menuInfo = document.querySelector('.menu');
 const two = document.querySelector('.insert_two');
 const offmenuInfo =  () => {
         const menuStoreStado = three.classList.contains('display');
+        const menuProduct = five.classList.contains('display');
+        const musprincipal = insert_fourt.classList.contains('display');
+      
+      
         if(!menuStoreStado){
                 three.classList.add('display');
         }
+        if(!menuProduct){
+                five.classList.add('display');
+        }
+        if(musprincipal){
+                insert_fourt.classList.toggle('display');
+                
+        }
+        if(!musprincipal){
+                insert_fourt.classList.add('display');
+                
+        }
+       /*  ofertas_container.classList.add('display'); */
         two.classList.toggle("display");
+        
 }
 
 
@@ -34,12 +55,17 @@ const three = document.querySelector('.insert_three');
 const offmenuStore = () => {
         const menuEmailStado = one.classList.contains("display"); // verifico si menu email esta abierto 
         const menuInfoStado = two.classList.contains('display');
-        console.log(menuEmailStado);
+        const menuProduct = five.classList.contains('display');
+      
+        
         if (!menuEmailStado){ // si esta false = abierton =>
                 one.classList.add('display') //  lo cierra 
         }
         if (!menuInfoStado){
                 two.classList.add('display');
+        }
+        if(!menuProduct){
+                five.classList.add('display');
         }
         three.classList.toggle('display'); // y abro menu carro 
 }
@@ -50,6 +76,41 @@ const offmenuStore = () => {
 menuEmail.addEventListener('click', offmenu); 
 menuInfo.addEventListener('click', offmenuInfo);
 menuStore.addEventListener('click', offmenuStore)
+
+
+/* ofertas  */
+
+const clsedd = document.querySelector('.close_product');
+const five = document.querySelector('.insert_five');
+
+function offmenuOfertas() {
+        
+        console.log( "Juan David" +"Castro Gallego")
+        const menuEmailStado = one.classList.contains("display"); // verifico si menu email esta abierto 
+        const menuInfoStado = two.classList.contains('display');
+        const menuStoreStado = three.classList.contains('display');
+        const menuProduct = five.classList.contains('display');
+
+        if (!menuEmailStado){ // si esta false = abierton =>
+                one.classList.add('display') //  lo cierra 
+        }
+        if (!menuInfoStado){
+                two.classList.add('display');
+        }
+        if(menuProduct){
+                five.classList.add('display');
+        }
+        if(!menuStoreStado){
+                three.classList.add('display');
+        }
+      
+        five.classList.toggle('display');
+}
+
+clsedd.addEventListener('click', offmenuOfertas);
+
+
+
 
 
 /*   crear las ofertas desde js para html  */
@@ -82,53 +143,73 @@ listaProductos.push({
 
 /* vamos a crear todo la maquetacion html  */
 const ofertas_container = document.querySelector('.ofertas_container');
+//const section_container= document,querySelector('.section_container');
 //const ofertas_container = document.querySelector('.insert_fourt');
-for (product of listaProductos){
+function listOfertasProductos(arr){
+        for (product of arr){
+                const productos = document.createElement('div');  // crea el elemento div
+                productos.classList.add('productos');             // le agrega la  clasee 'productos'
+            
+                const img_1 = document.createElement('img');
+                img_1.setAttribute('src', product.img);    // le cambia el atributo src por el q tenemos asignado en la lista 
+                img_1.classList.add('productos_img');
+                img_1.addEventListener('click', offmenuOfertas);  // como solo se creea cuando se ejecuta la pagina es como si no existiera nada 
+                                                                  // entonces se le pone el metodo   .addEventListener para cada elemento creado y poder ejecutar una fucion 
+            
 
-        
+                const productos_valor = document.createElement('div');
+                productos_valor.classList.add('productos_valor');
+            
+                const divProductos_v =document.createElement('div');
+                divProductos_v.classList.add('divProductos_v');
+            
+                const valorP= document.createElement('p');
+                valorP.classList.add('valor');
+                valorP.innerText= '$'+ product.valor;  //le agrega el simbolo $ concatenado con valor de la lita
+            
+                const valor_infoP= document.createElement('p');
+                valor_infoP.classList.add('valor_info');
+                valor_infoP.innerText=  product.name;
+                
+            
+                const logo_figure = document.createElement('figure');
+            
+                const logo_p= document.createElement('img');
+                logo_p.classList.add('logo_p');
+                logo_p.setAttribute('src','/imagen/logo_2.jpeg');  
+            
+                /* vamos a empezar a contcatenar  */
+            
+                logo_figure.appendChild(logo_p); // appendChild empieza a anidar 
+            
+                divProductos_v.appendChild(valorP);
+                divProductos_v.appendChild(valor_infoP);
+            
+                productos_valor.appendChild(divProductos_v);
+                productos_valor.appendChild(logo_figure);
+            
+                productos.appendChild(img_1);
+                productos.appendChild(productos_valor);
+            
+                ofertas_container.appendChild(productos);
+     
+        }
+}
 
-        const productos = document.createElement('div');  // crea el elemento div
-        productos.classList.add('productos');             // le agrega la  clasee 'productos'
-
-        const img_1 = document.createElement('img');
-        img_1.setAttribute('src', listaProductos.img);    // le cambia el atributo src por el q tenemos asignado en la lista 
-        img_1.classList.add('productos_img');
-
-        const productos_valor = document.createElement('div');
-        productos_valor.classList.add('productos-valor');
-
-        const divProductos_v =document.createElement('div');
-        divProductos_v.classList.add('divProductos_v');
-
-        const valorP= document.createElement('p');
-        valorP.classList.add('valor');
-        valorP.innerText= '$'+ listaProductos.valor;  //le agrega el simbolo $ concatenado con valor de la lita
-
-        const valor_infoP= document.createElement('p');
-        valor_infoP.classList.add('valor_info');
-        valor_infoP.innerText=  listaProductos.name;
-
-
-        const logo_figure = document.createElement('figure');
-
-        const logo_p= document.createElement('img');
-        logo_p.classList.add('logo_p');
-        logo_p.setAttribute('src','/imagen/logo_2.jpeg');  
-
-        /* vamos a empezar a contcatenar  */
-
-        logo_figure.appendChild(logo_p); // appendChild empieza a anidar 
-
-        divProductos_v.appendChild(valorP);
-        divProductos_v.appendChild(valor_infoP);
-
-        productos_valor.appendChild(divProductos_v);
-        productos_valor.appendChild(logo_figure);
-
-        productos.appendChild(img_1);
-        productos.appendChild(productos_valor);
-
-        ofertas_container.appendChild(productos);
+listOfertasProductos(listaProductos);
 
 
+
+
+
+/* productos  */
+
+const listaProductosCompra = [];
+
+const productos_valor =document.querySelector('.productos_valor');
+
+productos_valor.addEventListener('click', addcar);
+
+function addcar(){
+   console.log("productos_valor", productos_valor.Value);
 }
